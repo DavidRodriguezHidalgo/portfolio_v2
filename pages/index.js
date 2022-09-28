8a0ed5e3967bb5f27c176eff67d3f2fb32fe4f7d
David Rodriguez <denzilrh90@gmail.com>
2022-09-28 16:55:17 +0200

    add theme mode, navigation bar and sections

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        mb={6}
+        p={3}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in Spain!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -13,7 +26,30 @@ const Page = () => {
           </Heading>
           <p>Developer</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/david.jpeg"
+            alt="Profile image"
+          />
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
