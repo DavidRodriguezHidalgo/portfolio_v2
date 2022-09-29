81fc5bf2eb276a67d4f90df3c5edecc0496dcab8
David Rodriguez <denzilrh90@gmail.com>
2022-09-29 17:27:53 +0200

    fix: works title

--- pages/works.js
+++ pages/works.js
@@ -10,7 +10,7 @@ const Works = () => {
     <Layout>
       <Container>
         <Heading as="h3" fontSize={20} mb={4}>
-          Works Test
+          Works
         </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
