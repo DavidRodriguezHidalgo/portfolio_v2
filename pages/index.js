708e6d8f1abd114ef2a6ba9591517a6001a1ba6e
David Rodriguez <denzilrh90@gmail.com>
2022-09-29 16:58:37 +0200

    page transitions with framer motion

--- pages/index.js
+++ pages/index.js
@@ -8,93 +8,96 @@ import {
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        mb={6}
-        p={3}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Spain!
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            David Rodríguez
-          </Heading>
-          <p>Developer</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          mb={6}
+          p={3}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/david.jpeg"
-            alt="Profile image"
-          />
+          Hello, I&apos;m a full-stack developer based in Spain!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          David is a full-stack developer. For backend development I use Ruby on
-          Rails and React.js for frontend. What I like is to learn new things
-          and keep growing my skills. When not online, he loves bonsai care,
-          book reading and playing videogames.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme={'teal'}>
-              My portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              David Rodríguez
+            </Heading>
+            <p>Developer</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/david.jpeg"
+              alt="Profile image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1990</BioYear>
-          Born in Granada, Spain.
-        </BioSection>
-        <BioSection>
-          <BioYear>2014</BioYear>
-          Completed Computer Science Degree by Granada&apos;s University.
-        </BioSection>
-        <BioSection>
-          <BioYear>2014</BioYear>
-          Worked at Nazaries IT
-        </BioSection>
-        <BioSection>
-          <BioYear>2021</BioYear>
-          Worked at Camaloon
-        </BioSection>
-      </Section>
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>Books, Bonsai, Videogames, Photography.</Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            David is a full-stack developer. For backend development I use Ruby
+            on Rails and React.js for frontend. What I like is to learn new
+            things and keep growing my skills. When not online, he loves bonsai
+            care, book reading and playing videogames.
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme={'teal'}>
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1990</BioYear>
+            Born in Granada, Spain.
+          </BioSection>
+          <BioSection>
+            <BioYear>2014</BioYear>
+            Completed Computer Science Degree by Granada&apos;s University.
+          </BioSection>
+          <BioSection>
+            <BioYear>2014</BioYear>
+            Worked at Nazaries IT
+          </BioSection>
+          <BioSection>
+            <BioYear>2021</BioYear>
+            Worked at Camaloon
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>Books, Bonsai, Videogames, Photography.</Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,37 +1,39 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import Layout from '../components/layouts/article'
 import { WorkGridItem } from '../components/grid-item'
 import ThumbNazaries from '../public/images/works/nazaries_thumb.png'
 import ThumbCamaloon from '../public/images/works/camaloon_thumb.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="nazaries"
-            title="Nazaries IT"
-            thumbnail={ThumbNazaries}
-          >
-            Software development consultancy and Iot solutions
-          </WorkGridItem>
-        </Section>
-<Section>
-          <WorkGridItem
-            id="camaloon"
-            title="Camaloon"
-            thumbnail={ThumbCamaloon}
-          >
-            Personalized items and gifts.
-          </WorkGridItem>
-        </Section>
-
-      </SimpleGrid>
-    </Container>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works Test
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="nazaries"
+              title="Nazaries IT"
+              thumbnail={ThumbNazaries}
+            >
+              Software development consultancy and Iot solutions
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="camaloon"
+              title="Camaloon"
+              thumbnail={ThumbCamaloon}
+            >
+              Personalized items and gifts.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
